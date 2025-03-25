4f26a408376c05158b6892c1e515dca6a987be54
annayagyu <asotto4668@gmail.com>
2025-03-24 22:43:33 -0300

    initial commit

---
+++ src/main/java/com/github/acnaweb/study_apix/service/ProdutoService.java
@@ -0,0 +1,23 @@
+package com.github.acnaweb.study_apix.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.github.acnaweb.study_apix.model.Produto;
+
+@Service
+public class ProdutoService {
+
+    private List<Produto> produtos = new ArrayList<>();
+    private Long id = 1L;
+
+    public Produto save(Produto request) {
+
+        request.setId(id++);
+
+        return request;
+    }
+
+}
